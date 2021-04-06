0e58292777c9eb0019e268e461ac3d9d77ed3d4c
Undagi <67057021+Undagi@users.noreply.github.com>
2021-04-06 01:41:43 -0400

    Update server.java

--- server.java
+++ server.java
@@ -1,33 +1,42 @@
 import java.util.*;
 
 public class server {
-	
+
 	ArrayList<Double> holder;
 	double mu;
 	double nextdeparture;
-	
-	
+
+
 	public server(double exp) {
 		holder = new ArrayList<Double>();
 		mu = exp;
 		nextdeparture = Integer.MAX_VALUE;
 	}
-	
+
 
 	public boolean addto(double time, double arrival) {
 		holder.add(time);
-		
+
 		if(holder.size() == 1) {//means we need to generate a new departure time
 			nextdeparture = time + getexp();
 		}
-		
+
 		return false; //don't generate a new departure time
 	}
-	
-	
-	
-	
-	
+	public boolean addto2(double time, double arrival, double exp) {
+		holder.add(time);
+
+		if(holder.size() == 1) {//means we need to generate a new departure time
+			nextdeparture = time + exp;
+			return true;
+		}
+
+		return false; //don't generate a new departure time
+	}
+
+
+
+
 	public void output() {
 		System.out.print("Current Line: ");
 		for(int i = 0; i < holder.size(); i++) {
@@ -35,8 +44,8 @@ public class server {
 		}
 		System.out.println();
 	}
-	
-	
+
+
 	public double removefrom(double time) {
 		if(holder.size() == 1) {
 			nextdeparture = Integer.MAX_VALUE;
@@ -46,27 +55,42 @@ public class server {
 		}
 		//output();
 		//System.out.println(nextdeparture);
-		
+
 		return holder.remove(0);
 	}
-	
+	public double removefrom2(double time, double exp, boolean nochange) {
+		if(holder.size() == 1) {
+			nextdeparture = Integer.MAX_VALUE;
+			nochange = true;
+		}
+		else {
+			nextdeparture = time + exp;
+
+		}
+		//output();
+		//System.out.println(nextdeparture);
+
+		return holder.remove(0);
+	}
+
+
 	public double getnextdeparture() {
 		return nextdeparture;
 	}
-	
+
 	public int getlength() {
 		return holder.size();
 	}
-	
+
 	public double getexp() {
-		
-		
+
+
 		Random r = new Random();
 		return -(Math.log(1 - Math.random())/(mu));
-		
+
 	}
-	
-	
+
+
 	//two methods -> one in order to add to the list and one to take away
 
 }
